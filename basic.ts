f31ad6f6be3d6b3de436ee295b890b5de243ee12
uio64 <2692571183@qq.com>
2026-08-24 16:13:06 +0800

    修复水泵积木功率和时长参数

--- basic.ts
+++ basic.ts
@@ -2606,16 +2606,16 @@ namespace PlanetX_Basic {
         }
     }
 
-    //% blockId=airPump block="water pump module %Rjpin toggle to $operation || power %power \\% | duration %duration ms"
+    //% blockId=airPump block="water pump module %Rjpin toggle to $operation || power (%) %power | duration %duration ms"
     //% inlineInputMode=inline
     //% Rjpin.fieldEditor="gridpicker"
     //% Rjpin.fieldOptions.columns=2
     //% operation.shadow="toggleOnOff"
-    //% duration.min=0 duration.defl=1000
     //% power.min=0 power.max=100 power.defl=100
+    //% duration.min=0 duration.defl=1000
     //% expandableArgumentMode="enabled"
     //% subcategory=Execute group="Digital" color=#EA5532
-    export function airPump(Rjpin: DigitalRJPin, operation: boolean, duration: number = 1000, power: number = 100): void {
+    export function airPump(Rjpin: DigitalRJPin, operation: boolean, power: number = 100, duration: number = 1000): void {
         let pin = AnalogPin.P1
         switch (Rjpin) {
             case DigitalRJPin.J1:
